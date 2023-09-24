69098bee1a3b480bf7aa176ee479d23e92e7b53e
Erick Soto <99101149+mrrobot266@users.noreply.github.com>
2023-09-23 23:25:45 -0700

    Create +page.svelte
    Added an about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,22 @@
+<div class="about">
+    <body>
+        <a href="/home" class="tablink">Home Page</a>
+        <a href="/" class="tablink">Email Writing Tool</a>
+        <a href="/saveVoice" class="tablink">Save Voice Style</a>
+        <a href="/about" class="tablinkcurrent">About Page</a>
+    </body>
+
+    <h1 style="margin-bottom: 50px;">About Page</h1>
+
+    <h3>This application functions by allowing the user to select the email writer who has their corresponding email samples stored in a text file. The email samples are then analyzed by ChatGPT to replicate the user's unique writing style.</h3>
+</div>
+
+<style>
+    /* Set width to 100% and use flex display for horizontal alignment */
+    .about {
+        width: 100%;
+        text-align: center;
+        display: block;
+        justify-content: space-around; /* To evenly space the tab links */
+    }
+</style>
