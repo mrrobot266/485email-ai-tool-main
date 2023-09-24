18ff8d2472aaf55a4f5b716f8fd4e50753620466
Erick Soto <99101149+mrrobot266@users.noreply.github.com>
2023-09-23 23:27:14 -0700

    Create +page.svelte
    Added a home page you can navigate to

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+
+
+</script>
+
+  
+  <div class="home">
+      
+    <a href="/home" class="tablinkcurrent">Home Page</a>
+    <a href="/" class="tablink">Email Writing Tool</a>
+    <a href="/saveVoice" class="tablink">Save Voice Style</a>
+    <a href="/about" class="tablink">About Page</a>
+      
+  
+    <h1 style="margin-bottom: 50px;">Home Page</h1>
+    <h3>More content goes here...</h3>
+
+  </div>
+  
+<style>
+    /* Set width to 100% and use flex display for horizontal alignment */
+    .home {
+        width: 100%;
+        text-align: center;
+        display: block;
+        justify-content: space-around; /* To evenly space the tab links */
+    }
+</style>
