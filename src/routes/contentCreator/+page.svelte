27286ceaff18927fe0e2b7bef133bd0b5b4b25f8
Erick Soto <99101149+mrrobot266@users.noreply.github.com>
2023-11-04 15:59:19 -0700

    Update +page.svelte

--- src/routes/contentCreator/+page.svelte
+++ src/routes/contentCreator/+page.svelte
@@ -4,12 +4,59 @@
     import FieldWrapper from "../../components/field-wrapper.svelte";
     import Login from './login.svelte'; // Ensure the correct path
     import History from './history.svelte';
+    import { initializeApp } from 'firebase/app';
+    import { getFirestore, setDoc, getDocs, collection, Firestore } from 'firebase/firestore';
+    import { docStore } from "sveltefire";
+    import { getAnalytics } from "firebase/analytics";
+    import { onMount } from 'svelte';
     import ContentView from './contentView.svelte';
+
+    const firebaseConfig = {
+  apiKey: "AIzaSyAFxmdgTabKYliNNrZVj0s2XCFZPfwTyps",
+  authDomain: "touchpoint-capstone-database.firebaseapp.com",
+  projectId: "touchpoint-capstone-database",
+  storageBucket: "touchpoint-capstone-database.appspot.com",
+  messagingSenderId: "1032080279652",
+  appId: "1:1032080279652:web:9a53f2acb5069e91513771",
+  measurementId: "G-HH2QG68FLN"
+};
+
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore();
+   // const analytics = getAnalytics(app);
+
+async function getPersonas(db: Firestore) {
+  const writingStylesCollection  = collection(db, 'writingStyles');
+  const querySnapshot  = await getDocs(writingStylesCollection);
+  const writingStylesData  = querySnapshot .docs.map(doc => doc.data());
+  return writingStylesData ;
+}
+
+
+let writingStyles: any[] = [];
+
+onMount(async () => {
+        writingStyles = await getPersonas(db);
+    });
 
     let context = '';
     let requirement = '';
     let writingExample = '';
 
+
+    //new code
+    let targetAudience = '';
+    let keywords = '';
+    let tone = '';
+    let targetwordCount = '';
+    let yourName = '';
+
+    let context2 = '';
+    let loading2 = false;
+    let error2 = false;
+    let answer2 = '';
+    let copyDisabled2 = true;
+    
     let loading = false;
     let error = false;
     let answer = '';
@@ -20,12 +67,110 @@
     let showHistory = false;
 
     const handleSubmit = async () => {
-        // ... handleSubmit logic remains unchanged
-    };
+		loading = true
+		error = false
+		answer = ''
+		context = ''
+		context = "Create an outline for a comprehensive 3000-4000 word article about " + requirement + 
+		", give speculated word counts for each section. Write it for the target audience being: " + targetAudience;
 
-    const copyToClipboard = () => {
-        // ... copyToClipboard logic remains unchanged
-    };
+		const eventSource = new SSE('/api/explain2', { // create eventsource which opens Server sent events connection to endpoint
+			headers: {
+				'Content-Type': 'application/json' //indicates json data 
+			},
+			payload: JSON.stringify({ context })  // serializes the context variable as a JSON string and sends it as part of the request's payload data.
+		}) //returns text eventstream response
+
+		context = ''
+
+		eventSource.addEventListener('error', (e) => { //listens for errors
+			error = true
+			loading = false
+			alert('Something went wrong!')
+		})
+
+		eventSource.addEventListener('message', (e) => { //listens for new messages/tokens coming in, thats why new text continually shows up on screen
+			try {
+				loading = false             //no longer loading
+
+				if (e.data === '[DONE]') { //Completion of message request is terminated by 'Done'
+					copyDisabled = false;
+					return
+				}
+
+				const completionResponse: CreateCompletionResponse = JSON.parse(e.data)
+
+				const [{ text }] = completionResponse.choices  //The response from chatgpt is located in choices[text] section. first index of choices array
+                        // answer or '' if empty.
+				answer = (answer ?? '') + text  // Stream: tokens sent as data-only server-sent events as they become available
+			} catch (err) {
+				error = true
+				loading = false
+				console.error(err)
+				alert('Something went wrong!')
+			}
+		})
+
+		eventSource.stream()
+	}
+
+    const handleSubmitArt = async () => {
+		loading2 = true
+		error2 = false
+		answer2 = ''
+		context = ''
+		context = "Create an article based on this outline: " + answer +
+		", Write it in this writing style and tone: " + tone + ", and inlcude these keywords: " + keywords;
+
+		const eventSource = new SSE('/api/explain2', {
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			payload: JSON.stringify({ context })
+		})
+
+		context = ''
+
+		eventSource.addEventListener('error', (e) => {
+			error2 = true
+			loading2 = false
+			alert('Something went wrong!')
+		})
+
+		eventSource.addEventListener('message', (e) => {
+			try {
+				loading2 = false
+
+				if (e.data === '[DONE]') {
+					//copyDisabled = false;
+					return
+				}
+
+				const completionResponse: CreateCompletionResponse = JSON.parse(e.data)
+
+				const [{ text }] = completionResponse.choices
+
+				answer2 = (answer2 ?? '') + text
+			} catch (err) {
+				error2 = true
+				loading2 = false
+				console.error(err)
+				alert('Something went wrong!')
+			}
+		})
+
+		eventSource.stream()
+	}
+
+	const copyToClipboard = () => {
+		const elem = document.createElement('textarea')
+		elem.value = answer
+		document.body.appendChild(elem)
+		elem.select()
+		document.execCommand('copy')
+		document.body.removeChild(elem)
+		alert('Copied to clipboard!')
+ 	}
 </script>
 
 <header>
@@ -46,23 +191,73 @@
     <h1 class="text-3xl font-semibold">Write Me an Article</h1>
     <h2 class="text-sm text-dull my-6">Please fill out the details</h2>
     <form on:submit|preventDefault={handleSubmit} class="w-full p-4">
+        <FieldWrapper 
+        label="Tone & Style"
+        >
+        <div class="relative">
+            <select placeholder="Custom Tone" class="form-field w-full" bind:value={yourName}>
+                <option value="">Custom Tone</option>
+                {#each writingStyles as style}
+                <option value={style.personaName}>{style.personaName}</option>
+                {/each}
+            </select>
+            <span class="absolute right-4 top-5 arrow"/>
+        </div>
+
+        <input type="text" class="form-field w-full" placeholder="Ex: conversational, informative, energetic..." bind:value={tone}>
+        </FieldWrapper>
+
+        <FieldWrapper label="Target Audience">
+            <textarea class="form-field w-full h-10" name="target audience" bind:value={targetAudience} placeholder="Ex: Small businesses, Mothers, Students..."></textarea>
+        </FieldWrapper>
+
+        <FieldWrapper label="Keywords">
+            <textarea class="form-field w-full h-10" name="keywords" bind:value={keywords} placeholder="Ex: Innovation, Subscription, Pre-order..."></textarea>
+        </FieldWrapper>
+
         <FieldWrapper label="What is the article about?">
             <textarea class="form-field w-full h-20" name="requirement" bind:value={requirement} placeholder="Describe the article topic here"></textarea>
         </FieldWrapper>
         
-        <FieldWrapper label="Provide sample articles below so your writing style and tone can be replicated.">
-            <textarea class="form-field w-full h-52" id="email-textarea" name="writingExample" bind:value={writingExample}></textarea>
+        <button class="bg-secondary w-full p-4 rounded-md my-2">Write Outline for Article</button>
+        {#if answer}
+        <FieldWrapper 
+            label="Generated Outline"
+        >
+            <textarea 
+                class="form-field" 
+                rows="20" 
+                bind:value={answer} 
+                style="color: white;"
+            />
         </FieldWrapper>
-        <button class="bg-secondary w-full p-4 rounded-md my-2">Write Article</button>
+        <button on:click|preventDefault={copyToClipboard} class="bg-secondary w-full p-4 rounded-md my-2" disabled={copyDisabled}>Copy</button>
+        <button on:click|preventDefault={handleSubmitArt} class="bg-secondary w-full p-4 rounded-md my-2" >Generate Article</button>
+        <FieldWrapper 
+        label="Generated Article"
+    >
+        <textarea 
+            id = "Custom Article"
+            class="form-field" 
+            rows="20" 
+            bind:value={answer2} 
+            style="color: white;"
+        />
+        </FieldWrapper>
+    {/if}
     </form>
 
-    {#if answer}
+ 
+
+    <!-- {#if answer}
         <div class="my-8 border-[0] border-b border-line"></div>
         <FieldWrapper label="Generated Article:">
             <div class="form-field" innerHTML={answer}></div>
         </FieldWrapper>
         <button on:click|preventDefault={copyToClipboard} class="bg-secondary w-full p-4 rounded-md my-2" disabled={copyDisabled}>Copy</button>
-    {/if}
+    {/if} -->
+        
+   
 </div>
 
 <footer>
